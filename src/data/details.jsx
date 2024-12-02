dbd02a6a6edfcf998de67ac9c6201ac2b59fc5e5
Jagmeet Singh <149931452+Jagmeet5911@users.noreply.github.com>
2024-12-02 20:32:49 +0530

    Update details.jsx

--- src/data/details.jsx
+++ src/data/details.jsx
@@ -9,7 +9,7 @@ export const Bio = {
       "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
     github: "https://github.com/Jagmeet5911",
     resume:
-      "https://drive.google.com/file/d/1gv2xjxD8WPU9OD55v1Ngl54kkEEEZm6S/view?usp=drive_link",
+      "https://drive.google.com/file/d/1XhK_It2pXoLAK9WI-hQugQ3Bk4fxJEVr/view?usp=drive_link",
     linkedin: "https://www.linkedin.com/in/jagmeet-singh5911/",
     twitter: " ",
     insta: " ",
